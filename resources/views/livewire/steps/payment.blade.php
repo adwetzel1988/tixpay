cccdbbe178943f9e3b900dc5e1eb5c0b0ebfc78c
Jay Bryan Canas <jbryan.canas@gmail.com>
2024-10-26 13:18:01 +0800

    fix

--- resources/views/livewire/steps/payment.blade.php
+++ resources/views/livewire/steps/payment.blade.php
@@ -1,15 +1,13 @@
 <div>
-    <h1>test</h1>
+    <stripe-buy-button
+            buy-button-id="buy_btn_1QDwzrBhvPorK9JGR9Iv37So"
+            publishable-key="pk_test_51NcJ6pBhvPorK9JGtmLRFEr7DLeNHeEcaUhhiMKCUYZZ0521HtxVqWdJmgaRpoUyegHgrGaComCeCubEAIQjUsOP003rynfjUO"
+    >
+    </stripe-buy-button>
 </div>
 
 @assets
 <script async
   src="https://js.stripe.com/v3/buy-button.js">
 </script>
-
-<stripe-buy-button
-  buy-button-id="buy_btn_1QDwzrBhvPorK9JGR9Iv37So"
-  publishable-key="pk_test_51NcJ6pBhvPorK9JGtmLRFEr7DLeNHeEcaUhhiMKCUYZZ0521HtxVqWdJmgaRpoUyegHgrGaComCeCubEAIQjUsOP003rynfjUO"
->
-</stripe-buy-button>
 @endassets
